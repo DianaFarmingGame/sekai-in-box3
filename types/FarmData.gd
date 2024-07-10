3c678905a5f3dd3c6b99d9ee16f19ad26745d354
Lane Sun <lanesun@anlbrain.com>
2024-07-10 17:38:43 +0800

    07-10

---
+++ types/FarmData.gd
@@ -0,0 +1,9 @@
+class_name FarmData extends Resource
+
+enum WeatherType {
+	晴朗,
+	下雨,
+}
+
+var buildings
+var weather
---
+++ types/GlobalData.gd
@@ -0,0 +1,9 @@
+class_name GlobalData extends Resource
+
+var date
+var money
+var storage
+var toolbar
+var npc_approval
+var area_switch
+var function_switch
